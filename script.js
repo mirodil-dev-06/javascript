e29389835442afea0ffa699799aa82a898b976e5
Mirodil <mirodil.mavlonov.2022@gmail.com>
2024-11-26 23:19:45 +0500

    First commit

---
+++ script.js
@@ -0,0 +1,42 @@
+// Variables 
+// var surName = 'Mavlonov'
+// let name = 'Mirodil';
+// const age = 18;
+
+// Data types 
+// Primitiv
+// let name = 'Mirodil'; // String 
+// let age = 18; //Number
+// let isStudent = true;
+// let isMarried = false; // Boolean
+// let profession; //Undefined
+// let car = null; //Null
+// let bigNumber = BigInt(123456789012345678901234567890); //BigInt
+// let sym = Symbol("description"); //Symbol
+// Murakkab
+// const person = {
+//     name: 'Mirodil',
+//     age: 18,
+//     isStudent: true
+// }; // Key-value object
+// let nums = [1, 2, 3, 4, 5, 6] // Array
+// console.log(typeof "Ali"); // "string"
+// console.log(typeof 25); // "number"
+// console.log(typeof true); // "boolean"
+// console.log(typeof undefined); // "undefined"
+// console.log(typeof null); // "object" (bu JavaScriptdagi xato)
+// console.log(typeof Symbol("description")); // "symbol"
+// console.log(typeof BigInt(123456789012345678901234567890)); // "bigint"
+// console.log(typeof {}); // "object"
+// console.log(typeof []); // "object"
+// console.log(typeof function(){}); // "function"
+// console.log("5" + 5); // "55" (string)
+// console.log(5 + "5"); // "55" (string)
+// console.log("5" - 2); // 3 (number)
+// console.log("5" + "5"); // "55" (string)
+// console.log(5 - "3"); // 2 (number)
+// console.log("5" - "4"); // 1 (number)
+// let str = String(123); // "123"
+// let num = Number("123"); // 123
+// let bool = Boolean(1); // true
+
